2e7a687fc30653b04c0353906d77fdd70b97fcb2
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-27 13:20:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,100 @@
-let expenses = [];
-let totalAmount = 0;
+//1
+const balance = document.getElementById(
+    "balance"
+  );
+  const money_plus = document.getElementById(
+    "money-plus"
+  );
+  const money_minus = document.getElementById(
+    "money-minus"
+  );
+  const list = document.getElementById("list");
+  const form = document.getElementById("form");
+  const text = document.getElementById("text");
+  const amount = document.getElementById("amount");
 
-const categorySelect = document.getElementById('category-select');
-const amountInput = document.getElementById('amount-input');
-const dateInput = document.getElementById('date-input');
-const addBtn = document.getElementById('add-btn');
-const expensesTableBody = document.getElementById('expnese-table-body');
-const totalAmountCell = document.getElementById('total-amount');
-
-addBtn.addEventListener('click', function() {
-    const category = categorySelect.value;
-    const amount = Number(amountInput.value);
-    const date = dateInput.value;
-
-    if (category === '') {
-        alert('Please select a category');
-        return;
+  const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+  
+  let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+  
+  function addTransaction(e){
+    e.preventDefault();
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+      alert('please add text and amount')
+    }else{
+      const transaction = {
+        id:generateID(),
+        text:text.value,
+        amount:+amount.value
+      }
+  
+      transactions.push(transaction);
+  
+      addTransactionDOM(transaction);
+      updateValues();
+      updateLocalStorage();
+      text.value='';
+      amount.value='';
     }
-    if (isNaN(amount) || amount <=0 ) {
-        alert('Please enter a valid amoun')
-        return;
-    }
-    if(date === '') {
-        alert('Please select a date')
-        return;
-    }
-    expenses.push({category, amount, date});
-
-    totalAmount += amount;
-    totalAmountCell.textContent = totalAmount;
-
-    const newRow = expensesTableBody.insertRow();
-
-    const categoryCell = newRow.insertCell();
-    const amountCell = newRow.insertCell();
-    const dateCell = newRow.insertCell();
-    const deleteCell = newRow.insertCell();
-    const deleteBtn = document.createElement('button');
-
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.addEventListener('click', function() {
-        expenses.splice(expenses.indexOf(expense), 1);
-
-        totalAmount -= expense.amount;
-        totalAmountCell.textContent = totalAmount;
-
-        expensesTableBody.removeChild(newRow);
-    });
-
-    const expense = expenses[expenses.length - 1];
-    categoryCell.textContent = expense.category;
-    amountCell.textContent = expense.amount;
-    dateCell.textContent = expense.date;
-    deleteCell.appendChild(deleteBtn);
-
-});
-
-for (const expense of expenses) {
-    totalAmount += expense.amount;
-    totalAmountCell.textContent = totalAmount;
-
-    const newRow = expensesTableBody.inserRow();
-    const categoryCell = newRow.insertCell();
-    const amountCell = newRow.insertCell();
-    const dateCell = newRow.insertCell();
-    const deleteCell = newRow.insertCell();
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.addEventListener('click', function() {
-        expenses.splice(expenses.indexOf(expense), 1);
-
-        totalAmount -= expense.amount;
-        totalAmountCell.textContent = totalAmount;
-
-        expensesTableBody.removeChild(newRow);
-    });
-    categoryCell.textContent = expense.category;
-    amountCell.textContent = expense.amount;
-    dateCell.textContent = expense.date;
-    deleteCell.appendChild(deleteBtn);
-}
+  }
+  
+  function generateID(){
+    return Math.floor(Math.random()*1000000000);
+  }
+  function addTransactionDOM(transaction) {
+    const sign = transaction.amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+  
+    item.classList.add(
+      transaction.amount < 0 ? "minus" : "plus"
+    );
+  
+    item.innerHTML = `
+      ${transaction.text} <span>${sign}${Math.abs(
+      transaction.amount
+    )}</span>
+      <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+      `;
+    list.appendChild(item);
+  }
+  
+  function updateValues() {
+    const amounts = transactions.map(
+      (transaction) => transaction.amount
+    );
+    const total = amounts
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+    const income = amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+    const expense =
+      (amounts
+        .filter((item) => item < 0)
+        .reduce((acc, item) => (acc += item), 0) *
+      -1).toFixed(2);
+  
+      balance.innerText=`$${total}`;
+      money_plus.innerText = `$${income}`;
+      money_minus.innerText = `$${expense}`;
+  }
+  
+  function removeTransaction(id){
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    updateLocalStorage();
+    Init();
+  }
+  function updateLocalStorage(){
+    localStorage.setItem('transactions',JSON.stringify(transactions));
+  }
+  
+  function Init() {
+    list.innerHTML = "";
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+  }
+  
+  Init();
+  
+  form.addEventListener('submit',addTransaction);
